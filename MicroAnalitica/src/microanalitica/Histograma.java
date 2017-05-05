2d3197c98b4e10791ae62578dba855b086c13c24
famy <famy@famy-PC>
2017-05-04 21:36:14 -0500

    Se ha creado la clase histograma.

---
+++ MicroAnalitica/src/microanalitica/Histograma.java
@@ -0,0 +1,35 @@
+package microanalitica;
+
+public class Histograma {
+    
+    Pixel[][] miImagen= new Pixel[5][5];
+    
+    int [] R= new int[256];
+    int [] G= new int[256];
+    int [] B= new int[256];
+    int canal;
+            
+    void crearHistograma(){
+        
+        int n;
+        int m;
+        
+        for(n=0;n<256;n++){
+            for(m=0;m<256;m++){
+                switch(canal){
+                    case 0:{
+                        
+                    }
+                    case 1:{
+                        
+                    }
+                    default:{
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+    
+}
